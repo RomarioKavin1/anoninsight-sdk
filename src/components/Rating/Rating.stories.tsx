62e111ed7aaba17fc3ebadacbbf1a7ed091ce117
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-02-17 01:48:15 +0530

    build: features

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Rating } from './Rating';
 
-storiesOf('Rating', module).add('Default', () => <Rating groupid='nfajf' />);
+storiesOf('Rating', module).add('Default', () => <Rating groupid='17997299276220339278453471448622' apikey='d6a0b754-cd02-4628-9798-5009a5f5f534'/>);
